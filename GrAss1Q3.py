646e5b78a35f7b8e1d7ae7096d26f7143500de71
Kwasi-F <ioafrimpong@gmail.com>
2021-06-10 21:25:12 +0000

    line update

--- GrAss1Q3.py
+++ GrAss1Q3.py
@@ -16,3 +16,4 @@ print(even_numbers(10))  # Should be 2 4 6 8 10
 print(even_numbers(1))  # No numbers displayed
 print(even_numbers(3))  # Should be 2
 print(even_numbers(0))  # No numbers displayed
+print(even_numbers(12))
